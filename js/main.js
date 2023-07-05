36657e3998be8b4c4a40b8cbbf88c65f1feee836
LeonardoPoltava <vladyslavkrp@gmail.com>
2023-07-05 23:15:02 +0300

    technologies page

--- js/main.js
+++ js/main.js
@@ -14,23 +14,25 @@ document.querySelector(".mobile-nav__btn").addEventListener("click", function (e
 const buttons = document.querySelectorAll('.bases-tabs__elem[data-tab]');
 
 // Add click event listener to each button
-buttons.forEach(button => {
-    button.addEventListener('click', function () {
-        // Get the data-tab value of the clicked button
-        const tabName = this.getAttribute('data-tab');
+if(buttons.length) {
+    buttons.forEach(button => {
+        button.addEventListener('click', function () {
+            // Get the data-tab value of the clicked button
+            const tabName = this.getAttribute('data-tab');
 
-        // Get the block with the corresponding data-tab value
-        const block = document.querySelector(`[data-tab="${tabName}"]`);
+            // Get the block with the corresponding data-tab value
+            const block = document.querySelector(`section[data-tab="${tabName}"]`);
 
-        // Remove the 'active' class from all buttons and blocks
-        buttons.forEach(btn => btn.classList.remove('bases-tabs__elem--active'));
-        document.querySelectorAll('[data-tab-block]').forEach(blk => blk.classList.remove('bases-icons__list--active'));
-
-        // Add the 'active' class to the clicked button and block
-        this.classList.add('bases-tabs__elem--active');
-        block.classList.add('bases-icons__list--active');
+            // Remove the 'active' class from all buttons and blocks
+            buttons.forEach(btn => btn.classList.remove('bases-tabs__elem--active'));
+            document.querySelectorAll('section[data-tab-block]').forEach(blk => blk.style.display = 'none');
+            console.log(block);
+            // Add the 'active' class to the clicked button and block
+            this.classList.add('bases-tabs__elem--active');
+            block.style.display = 'grid';
+        });
     });
-});
+}
 // swiper
 const swiper = new Swiper('.swiper', {
     loop: true,
